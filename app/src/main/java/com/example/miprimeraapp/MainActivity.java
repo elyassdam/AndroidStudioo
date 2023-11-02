1a87cea4f890574581f6a89888bc94a0a2ce19fa
damSegundo <yassin@iesvalmayor.es>
2023-11-02 21:35:32 +0100

    21:35 02/11/23

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -8,15 +8,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
     private EditText editTextNumber1;
     private EditText editTextNumber2;
     private TextView resultadoTextView;
     private Button btnSumar;
-    private Button btnresta;
-    private Button btnmulti;
-    private Button btndivide;
+    private Button btnRestar;
+    private Button btnMultiplicar;
+    private Button btnDividir;
 
+    FirebaseFirestore db ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +31,12 @@ public class MainActivity extends AppCompatActivity {
         editTextNumber2 = findViewById(R.id.editTextNumber2);
         resultadoTextView = findViewById(R.id.resultado);
         btnSumar = findViewById(R.id.btsuma);
-        btnresta = findViewById(R.id.btresta);
-        btnmulti = findViewById(R.id.btmultip);
-        btndivide=findViewById(R.id.btdivide);
+        btnRestar = findViewById(R.id.btresta);
+        btnMultiplicar = findViewById(R.id.btmultip);
+        btnDividir = findViewById(R.id.btdivide);
 
+        // Inicializa la referencia a la base de datos de Firebase
+       db= FirebaseFirestore.getInstance();
     }
 
     public void sumar(View view) {
@@ -38,43 +45,49 @@ public class MainActivity extends AppCompatActivity {
         int numero1 = Integer.parseInt(numero1Str);
         int numero2 = Integer.parseInt(numero2Str);
         int resultado = numero1 + numero2;
-        resultadoTextView.setText(String.valueOf(resultado));
 
+        // Guarda el resultado en Firebase
+        databaseReference.child("resultados").push().setValue(resultado);
+        resultadoTextView.setText(String.valueOf(resultado));
     }
 
-    public void resta(View view) {
+    public void restar(View view) {
         String numero1Str = editTextNumber1.getText().toString();
         String numero2Str = editTextNumber2.getText().toString();
-
         int numero1 = Integer.parseInt(numero1Str);
         int numero2 = Integer.parseInt(numero2Str);
         int resultado = numero1 - numero2;
+
+        // Guarda el resultado en Firebase
+        databaseReference.child("resultados").push().setValue(resultado);
         resultadoTextView.setText(String.valueOf(resultado));
-
-
     }
 
-    public void multi(View view) {
+    public void multiplicar(View view) {
         String numero1Str = editTextNumber1.getText().toString();
         String numero2Str = editTextNumber2.getText().toString();
         int numero1 = Integer.parseInt(numero1Str);
         int numero2 = Integer.parseInt(numero2Str);
         int resultado = numero1 * numero2;
-        resultadoTextView.setText(String.valueOf(resultado));
 
+        // Guarda el resultado en Firebase
+        databaseReference.child("resultados").push().setValue(resultado);
+        resultadoTextView.setText(String.valueOf(resultado));
     }
 
-    public void Divide(View view) {
+    public void dividir(View view) {
         try {
             String numero1Str = editTextNumber1.getText().toString();
             String numero2Str = editTextNumber2.getText().toString();
             int numero1 = Integer.parseInt(numero1Str);
             int numero2 = Integer.parseInt(numero2Str);
             int resultado = numero1 / numero2;
+
+            // Guarda el resultado en Firebase
+            databaseReference.child("resultados").push().setValue(resultado);
             resultadoTextView.setText(String.valueOf(resultado));
         } catch (Exception e) {
             resultadoTextView.setText("ERROR");
-
         }
     }
 }
